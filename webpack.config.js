a5aa2066a56e8bb7451b82b33279d8b36caed285
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2018-07-23 18:04:43 +0300

    init env

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+'use strict'
+var webpack = require('webpack')
+// var LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+var env = process.env.NODE_ENV
+
+var reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react'
+}
+
+var reduxExternal = {
+  root: 'Redux',
+  commonjs2: 'redux',
+  commonjs: 'redux',
+  amd: 'redux'
+}
+
+var reactReduxExternal = {
+  root: 'ReactRedux',
+  commonjs2: 'react-redux',
+  commonjs: 'react-redux',
+  amd: 'react-redux'
+}
+
+var config = {
+  externals: {
+    react: reactExternal,
+    redux: reduxExternal,
+    'react-redux': reactReduxExternal
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'ReduxForm',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    // new LodashModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
+}
+
+if (env === 'production') {
+  config.mode = 'production'
+}
+
+module.exports = config
