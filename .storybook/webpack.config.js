a5999d44b5d9daf183268e7ec65bde1b7a026bcb
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2018-07-31 20:14:20 +0300

    add styles loaders

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = (storybookBaseConfig, configType) => {
+  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  storybookBaseConfig.module.rules.push({
+    test: /\.scss$/,
+    loaders: [
+      require.resolve('style-loader'),
+      {
+        loader: require.resolve('css-loader'),
+        options: {
+          importLoaders: 1,
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+      require.resolve('sass-loader')
+    ],
+  });
+
+  // Return the altered config
+  return storybookBaseConfig;
+};
