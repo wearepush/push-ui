f56e0dbc093b3ac27dd7ea77dca8849d4e31588a
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2018-08-01 18:29:45 +0300

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-'use strict'
-var webpack = require('webpack')
-// var LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
-var env = process.env.NODE_ENV
+'use strict';
+var webpack = require('webpack');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var env = process.env.NODE_ENV;
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 var reactExternal = {
   root: 'React',
   commonjs2: 'react',
   commonjs: 'react',
   amd: 'react'
-}
+};
 
 var reduxExternal = {
   root: 'Redux',
   commonjs2: 'redux',
   commonjs: 'redux',
   amd: 'redux'
-}
+};
 
 var reactReduxExternal = {
   root: 'ReactRedux',
   commonjs2: 'react-redux',
   commonjs: 'react-redux',
   amd: 'react-redux'
-}
+};
 
 var config = {
   externals: {
@@ -35,23 +35,98 @@ var config = {
   mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: env === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          },
+          {
+            loader : 'css-loader',
+            options:
+            {
+              sourceMap: true,
+              importLoaders: 2,
+              modules: true,
+              localIdentName: '[local]' // __[hash:base64:5]
+            }
+          },
+          {
+            loader : 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader : 'sass-loader',
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader : 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
       { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
     ]
   },
   output: {
-    library: 'ReduxForm',
+    library: 'ReduxStarterUi',
     libraryTarget: 'umd'
   },
   plugins: [
-    // new LodashModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+      chunkFilename: "index.chunk.css"
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
     })
   ]
-}
+};
 
 if (env === 'production') {
-  config.mode = 'production'
+  config.mode = 'production';
+
+  config.optimization = {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        uglifyOptions: {
+          output: {
+            comments: false,
+            beautify: false,
+          }
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          discardComments: {
+            removeAll: true
+          }
+        }
+      })
+    ]
+  };
 }
 
-module.exports = config
+module.exports = config;
