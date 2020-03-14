c8a48dfe80bdb79ae5bc59175ec3fbd9b74a4bbb
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2020-03-14 12:45:36 +0200

    chore: add react-scripts

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,7 @@ module.exports = {
   }],
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
   addons: [
-    '@storybook/preset-create-react-app',
+    // '@storybook/preset-create-react-app',
     '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-links',
