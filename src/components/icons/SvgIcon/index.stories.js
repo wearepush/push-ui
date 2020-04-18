f58f06d0f7c7f92f021dd4a1730836c39e1e3529
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2020-04-18 13:44:12 +0300

    feat(svgicon component): add svgicon component

--- src/components/icons/SvgIcon/index.stories.js
+++ src/components/icons/SvgIcon/index.stories.js
@@ -5,24 +5,6 @@ import SvgIcon from './SvgIcon';
 
 storiesOf('icons/SvgIcon', module)
   .addDecorator(withKnobs)
-  // .add('BAELogo', () => (
-  //   <SvgIcon
-  //     name="BAELogo"
-  //     viewBox="0 0 34 34"
-  //     width={select('width', ['12', '21', '22', '34', '42'], '34')}
-  //     height={select('height', ['10', '20', '21', '34', '40'], '34')}
-  //     fill={select('fill', ['#2D2D3C', '#EEEEEF'], '#2D2D3C')}
-  //   />
-  // ))
-  // .add('messaging', () => (
-  //   <SvgIcon
-  //     name="messaging"
-  //     viewBox="0 0 20 20"
-  //     width={select('width', ['14', '18', '20'], '20')}
-  //     height={select('height', ['14', '18', '20'], '20')}
-  //     fill={select('fill', ['#2D2D3C', '#292D3B', '#7F7F88', '#FA6428'], '#2D2D3C')}
-  //   />
-  // ))
   .add('profile', () => (
     <SvgIcon
       name="profile"
--- src/components/icons/SvgIcon/svgs/index.js
+++ src/components/icons/SvgIcon/svgs/index.js
@@ -1,9 +1,5 @@
-import BAELogo from './BAELogo.svg';
-import messaging from './messaging.svg';
 import profile from './profile.svg';
 
 export default {
-  BAELogo,
-  messaging,
   profile,
 };
