b01f982d19cc5616d68603560f59bebeb67b9e12
Kirill <kirill@wearepush.co>
2021-01-22 17:42:46 +0200

    feat: update textfield styles

--- src/components/inputs/TextField/TextField.style.js
+++ src/components/inputs/TextField/TextField.style.js
@@ -1,40 +1,25 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import { variant } from 'styled-system';
 
-export const sizeVariant = variant({
-  prop: 'size',
-  scale: 'inputSizes',
-  variants: {
-    xs: {},
-    sm: {},
-    md: {},
-    lg: {},
-    xl: {},
-  },
-});
-
-export const inputStyle = ({ theme: { input } }) =>
+export const inputStyle = () =>
   css`
-    background-color: ${input.backgroundColor};
+    background-color: var(--color--white);
     border: 1px solid;
-    border-color: ${input.borderColor};
-    border-radius: ${input.borderRadius};
+    border-color: var(--color--gray400);
+    border-radius: 3px;
     box-sizing: border-box;
-    color: ${input.color};
+    color: var(--color--black900);
     display: block;
-    fontsize: ${input.fontSize};
     font-weight: 400;
     line-height: 1.5;
-    padding: ${input.padding};
-    transition: ${input.transition};
+    transition: border-color 0.25s ease-in-out, background-color 0.25s ease-in-out;
     width: 100%;
     -webkit-appearance: none;
     -webkit-font-smoothing: antialiased;
 
     &:focus {
-      border-color: ${input.borderColorFocus};
-      outline: ${input.outlineFocus};
+      border-color: var(--color--gray900);
+      outline: none;
     }
     &:disabled {
       background-color: var(--color--gray300);
@@ -42,7 +27,7 @@ export const inputStyle = ({ theme: { input } }) =>
       cursor: not-allowed;
     }
     &::placeholder {
-      color: ${input.placeholderColor};
+      color: #a6aebc;
     }
     &.is-invalid {
       border-color: var(--border__color__invalid);
@@ -52,9 +37,15 @@ export const inputStyle = ({ theme: { input } }) =>
     }
   `;
 
+const inputSizes = (props) =>
+  css`
+    padding: var(--input--padding--${props.size});
+    font-size: var(--input--fontsize--${props.size});
+  `;
+
 export const StyledTextField = styled.input`
   ${inputStyle}
-  ${sizeVariant}
+  ${inputSizes}
 `;
 
 export default {
